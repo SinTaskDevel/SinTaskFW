ee9e496669519e09229f0e3aa2c7af744daa7e14
sintask <developer@sintask.com>
2018-09-22 18:51:11 +0800

    update responsive basic css

--- assets/page/static/dcss/misc.php
+++ assets/page/static/dcss/misc.php
@@ -33,3 +33,337 @@
     letter-spacing: 0px;
     padding: 2px 2px 2px 2px;
 }
+
+/* For Responsive */
+.w_fullContentR {
+    width: 100%;
+    box-sizing: border-box;
+}
+.w_twoContentR {
+    width: 50%;
+    box-sizing: border-box;
+}
+.w_threeContentR {
+    width: 33%;
+    box-sizing: border-box;
+}
+
+/* CSSD-SFW */
+body.selteer {
+    background: #FFF;
+    background-repeat: repeat;
+    background-position: center center;
+    background-attachment: fixed;
+    line-height: 1.4em;
+}
+
+h1.selteer, h2.selteer, h3.selteer {
+    letter-spacing: -1px;
+}
+p.selteer {
+    letter-spacing: 0.1px;
+    line-height: 1.4em;
+}
+div.selteer[stdll] {
+    letter-spacing: 0.1px;
+    line-height: 1.4em;
+}
+
+h2.headSelteer {
+    color: #45898F;
+    background: #FFFFFF;
+    font-family: Arial;
+    font-size: 22px;
+    border-bottom: 1px solid #45898F;
+    font-weight: lighter;
+    letter-spacing: -1px;
+    padding: 2% 15%;
+}
+h2.titleSelteer {
+    color: #FFFFFF;
+    background: #45898F;
+    font-family: Arial;
+    font-size: 20px;
+    font-weight: bold;
+    letter-spacing: -1px;
+    padding: 20px 20px;
+}
+div.contentAreaSelteer {
+    padding: 40px 15%;
+    margin: 0px auto;
+    font-size: 16px;
+    color: #FFF;
+    background: linear-gradient(60deg, rgb(53, 92, 125), rgb(108, 91, 123), rgb(211, 144, 86));
+}
+div.contentAreaSelteer[type='2'] {
+    padding: 40px 15%;
+    margin: 0px auto;
+    font-size: 16px;
+    color: #555;
+    background: #FAFAFA;
+}
+div.contentAreaSelteer[type='3'] {
+    padding: 40px 15%;
+    margin: 0px auto;
+    font-size: 16px;
+    color: #555;
+    background: #EAEAEA;
+}
+div.contentAreaTwoSideSelteer {
+    box-sizing: border-box;
+    width: 50%;
+    position: relative;
+    float: left;
+    padding: 5px;
+}
+div.contentAreaTwoSideSelteer > img[art-image] {
+    margin-left: 15%;
+    width: 300px;
+}
+
+div.contentAreaThreeSideSelteer {
+    box-sizing: border-box;
+    width: 33%;
+    position: relative;
+    float: left;
+    padding: 10px;
+}
+div.contentAreaThreeSideSelteer > .innerContent[color='1'] {
+    background: #A0B4C7;
+    border-radius: 4px;
+    padding: 20px;
+}
+div.contentAreaThreeSideSelteer > .innerContent[color='2'] {
+    background: #4989A2;
+    border-radius: 4px;
+    padding: 20px;
+}
+div.contentAreaThreeSideSelteer > .innerContent[color='3'] {
+    background: #80A774;
+    border-radius: 4px;
+    padding: 20px;
+}
+div.contentAreaThreeSideSelteer > .innerContent[color='4'] {
+    background: #5AAEAE;
+    border-radius: 4px;
+    padding: 20px;
+}
+div.contentAreaThreeSideSelteer > .innerContent[color='5'] {
+    background: #607d8b;
+    border-radius: 4px;
+    padding: 20px;
+}
+div.contentAreaThreeSideSelteer > .innerContent[color='6'] {
+    background: #ECA945;
+    border-radius: 4px;
+    padding: 20px;
+}
+div.contentAreaThreeSideSelteer > .innerContent[color='7'] {
+    background: #7527b0;
+    border-radius: 4px;
+    padding: 20px;
+}
+div.contentAreaThreeSideSelteer > .innerContent[color='8'] {
+    background: #E91E63;
+    border-radius: 4px;
+    padding: 20px;
+}
+div.contentAreaThreeSideSelteer > .innerContent > [prodBody] {
+    min-height: 250px;
+    max-height: 250px;
+}
+div.contentAreaThreeSideSelteer > .innerContent > [prodFooter] {
+    margin-top: 10px;
+}
+
+div.contentTwoSelteer {
+    padding: 20px;
+}
+div.borderLineSelteer {
+    margin-top: 15px;
+    padding-top: 15px;
+    border-top: 1px solid #DADADA;
+}
+div.borderSpaceMiniSelteer {
+    margin-top: 5px;
+    padding-top: 5px;
+}
+div.borderSpaceMiniSuperSelteer {
+    margin-top: 5px;
+}
+div.borderSpaceSelteer {
+    margin-top: 20px;
+    padding-top: 20px;
+}
+span.thisTaggingSelteer {
+    font-size: 13px;
+    padding: 1px 5px;
+    border: 2px solid #EAEAEA;
+    background: #EAEAEA;
+    color: #555;
+    font-weight: bolder;
+    border-radius: 4px;
+}
+.notedSelteer {
+    padding: 15px;
+    border: 1px solid #EA9F2D;
+    color: #555;
+}
+.orangeBubbleSelteer {
+    font-size: 13px;
+    padding: 1px 5px;
+    border: 2px solid #EA9F2D;
+    background: #EA9F2D;
+    color: #FFF;
+    font-weight: bolder;
+    border-radius: 2px;
+    display: inline-block;
+}
+input.selteer[type='submit'] {
+    padding: 2px 6px;
+    font-size: 13px;
+    border: 2px solid #DADADA;
+    border-radius: 4px;
+    color: #555;
+    margin-top: 2px;
+    margin-bottom: 2px;
+    cursor: pointer;
+    transition: all 0.2s linear 0s;
+}
+input.selteer[type='submit']:hover {
+    box-shadow: 0px 0px 4px #111;
+}
+ul.selteer {
+    margin-left: 20px;
+}
+
+input.toastSelteer {
+    padding: 5px 15px;
+    border-radius: 2px;
+    border: 1px solid #555;
+    width: 100%;
+    box-sizing: border-box;
+    margin-bottom: 2px;
+}
+button.toastSelteer {
+    border: 2px solid #555;
+    border-radius: 3px;
+    padding: 5px 15px;
+    display: inline-block;
+    margin-right: 5px;
+    margin-bottom: 2px;
+}
+input.selteer[top] {
+    border: 2px solid #FFFFFF;
+    padding: 10px 20px;
+    color: #555555;
+    width: 100%;
+    box-sizing: border-box;
+    border-bottom: 2px solid #DADADA;
+    border-radius: 6px 6px 0px 0px;
+}
+input.selteer[middle] {
+    border: 2px solid #FFFFFF;
+    padding: 10px 20px;
+    color: #555555;
+    width: 100%;
+    box-sizing: border-box;
+    border-bottom: 2px solid #DADADA;
+}
+input.selteer[bottom] {
+    border: 2px solid #FFFFFF;
+    padding: 10px 20px;
+    color: #555555;
+    width: 100%;
+    box-sizing: border-box;
+    border-radius: 0px 0px 6px 6px;
+}
+
+button.selteer[std-one] {
+    border: 2px solid #FFFFFF;
+    padding: 10px 20px;
+    color: #67B26F;
+    font-weight: bold;
+    cursor: pointer;
+    background: #FFFFFF;
+    width: 100%;
+    box-sizing: border-box;
+    border-radius: 6px;
+}
+button.selteer[std-two] {
+    border: 2px solid #FFFFFF;
+    padding: 10px 20px;
+    color: #FFFFFF;
+    font-weight: bold;
+    cursor: pointer;
+    background: transparent;
+    width: 100%;
+    box-sizing: border-box;
+    border-radius: 6px;
+}
+button.selteer[std-three-bottom] {
+    border: 2px solid #FFFFFF;
+    padding: 10px 20px;
+    color: #FFFFFF;
+    font-weight: bold;
+    cursor: pointer;
+    background: transparent;
+    width: 100%;
+    box-sizing: border-box;
+    border-radius: 6px;
+    bottom: 0;
+}
+.theFooter {
+    border-top: 1px solid #EAEAEA;
+    position: absolute;
+    text-align: left;
+    padding: 2% 15%;
+    background: #FFF;
+    font-size: 13px;
+    width: 100%;
+    box-sizing: border-box;
+}
+
+.justifyAlignSelteer {
+    text-align: justify;
+}
+
+@media only screen and (max-width: 1000px) {
+    div.contentAreaThreeSideSelteer {
+        width: 50%; 
+    }
+}
+@media only screen and (max-width: 700px) {
+    .headerSintaskFrameworkSelteer {
+        margin: 2px 1% 2px 3%;
+    }
+    div.contentAreaSelteer {
+        padding: 40px 7%;
+    }
+    div.contentAreaSelteer[type='2'] {
+        padding: 40px 7%;
+    }
+    div.contentAreaTwoSideSelteer {
+        width: 100%;    
+    }
+    div.contentAreaTwoSideSelteer > img[art-image] {
+        margin-left: 20%;
+        width: 250px;
+    }
+    .theFooterSelteer {
+        padding: 40px 7%;
+    }
+    div.contentAreaThreeSideSelteer {
+        width: 100%;    
+    }
+    .w_threeContentR {
+        width: 50%;
+        box-sizing: border-box;
+    }
+}
+@media only screen and (max-width: 500px) {
+    .w_twoContentR, .w_threeContentR {
+        width: 100%;
+        box-sizing: border-box;
+    }
+}
